aac72e2fe39c27b99759ca4cff399c01dc891aff
hoetmaaiers <robin.houdmeyers@gmail.com>
2020-08-10 07:15:40 +0200

    Add Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@zeit-ui/react/esm/button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <Button>Special button</Button>
       </header>
     </div>
   );
